c013b2f2d5dddce9b0fe48da440a4668d0e0e0af
Thomaz Barros <thomazavila@gmail.com>
2023-10-01 17:06:26 -0300

    Now I can get a new deck.

--- src/cards/main.go
+++ src/cards/main.go
@@ -1,19 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card string = "Ace of Spades"
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards := newDeck()
+	cards.print()
 }
