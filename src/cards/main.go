266fb7b1cd10ee695eea267002544dad9f331695
Thomaz Barros <thomazavila@gmail.com>
2023-10-07 22:38:44 -0300

    We may save the deck to a file.

--- src/cards/main.go
+++ src/cards/main.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	//var card string = "Ace of Spades"
 	cards := newDeck()
+	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	// fmt.Println([]byte("Fim"))
 }
