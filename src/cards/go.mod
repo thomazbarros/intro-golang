9b8b678353de8c4ca3eddb8e5c76d57ee8ba4bba
Thomaz Barros <thomazavila@gmail.com>
2024-04-27 22:51:39 -0300

    My first go test.

--- src/cards/go.mod
+++ src/cards/go.mod
@@ -1,3 +1,3 @@
-module cards
+module cargs
 
-go 1.21.0
+go 1.22.2
--- src/cards/main.go
+++ src/cards/main.go
@@ -2,10 +2,10 @@ package main
 
 func main() {
 	//var card string = "Ace of Spades"
-	// cards := newDeck()
-	// fmt.Println(cards.toString())
-	// cards.saveToFile("my_cards")
 	cards := newDeck()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
+	//cards := newDeck()
 	cards.shuffle()
 	cards.print()
 
