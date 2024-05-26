16905d464f2e7af2e64a9762e8f845719fec1427
Thomaz Avila Barros <thomazavilabarros@MacBook-Pro-de-Thomaz.local>
2024-05-26 13:01:41 -0300

    Dealing with pointers.

--- src/structs/main.go
+++ src/structs/main.go
@@ -10,7 +10,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -18,11 +18,22 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.print()
+	fmt.Println()
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
