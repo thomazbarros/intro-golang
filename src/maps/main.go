4ab3e17ca7c0ab0dbcee98004fc70b0620676ece
Thomaz Avila Barros <thomazavilabarros@MacBook-Pro-de-Thomaz.local>
2024-05-26 22:38:43 -0300

    More options to create a map.

--- src/maps/main.go
+++ src/maps/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	colors["white"] = "#fffff"
+
 	fmt.Println(colors)
 
 }
