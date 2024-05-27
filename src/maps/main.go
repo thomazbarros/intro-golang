8ce16c47df6cb8b59c23f21e5e6d3a07095af105
Thomaz Avila Barros <thomazavilabarros@MacBook-Pro-de-Thomaz.local>
2024-05-26 22:11:48 -0300

    First map.

--- src/maps/main.go
+++ src/maps/main.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	colors := map[string]string{
-		"red":"#ff0000",
-		"green":
+		"red":   "#ff0000",
+		"green": "#4bf745",
 	}
-}
+	fmt.Println(colors)
 
+}
